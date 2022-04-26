462fe283608b46092e721a853dd348c3c7bd9bb9
Animu19 <103195812+Animu19@users.noreply.github.com>
2022-04-26 12:48:19 +0300

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,2 +1,28 @@
 public class Animal {
+
+   public void run (int length,String name,int maxrun){
+       if(maxrun==200)
+       if(length<=maxrun)
+            System.out.println(name+" пробежал(а):"+length+" метров.");
+       else {
+           System.out.println(name+" не смог(ла) пробежать:"+length+" метров и устал(а).");
+       }
+       if(maxrun==500)
+       if(length<=maxrun)
+           System.out.println(name+" пробежал(а):"+length+" метров.");
+       else {
+           System.out.println(name+" не смог(ла) пробежать:"+length+" метров и устал(а).");
+       }
+    }
+    public void swim(int length,String name,int maxswim){
+       if(maxswim==10)
+           if (length<=maxswim)
+        System.out.println(name+" проплыл(а):"+length+" метров");
+       else{
+            System.out.println(name+" не смог(ла) проплыть:"+length+" метров и устал(а).");
+        }
+       if(maxswim==0)
+           System.out.println(name+" не смог(ла), т.к коты(кошки), не умеют плавать.");
+    }
+
 }
