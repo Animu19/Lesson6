e3e1973d2c8aa9bd33ad193d1f2fa1b5d5c922d5
Animu19 <103195812+Animu19@users.noreply.github.com>
2022-04-26 12:50:36 +0300

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,3 +1,13 @@
 public class Cat extends Animal {
+    String name;
+    String color;
+    int age;
+    int maxrun = 200;
+    int maxswim =0;
 
+    public Cat(String name, String color, int age){
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
 }
